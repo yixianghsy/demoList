2518d49b09523a856b2775aae928f183a9a6d4e7
yixianghsy <yixianghsy@gmail.com>
2024-07-10 23:06:31 +0800

    5

---
+++ spring-step/src/main/java/cn/bugstack/springframework/beans/factory/ConfigurableListableBeanFactory.java
@@ -0,0 +1,2 @@
+package cn.bugstack.springframework.beans.factory;public class ConfigurableListableBeanFactory {
+}
---
+++ spring-step/src/main/java/cn/bugstack/springframework/beans/factory/ListableBeanFactory.java
@@ -0,0 +1,4 @@
+package cn.bugstack.springframework.beans;
+
+public class ListableBeanFactory {
+}
