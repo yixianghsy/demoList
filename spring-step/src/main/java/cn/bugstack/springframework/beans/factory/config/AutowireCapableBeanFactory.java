ae3e389fd096e6687c0df605844c2fcb2837cfcf
yixianghsy <yixianghsy@gmail.com>
2024-12-12 09:16:01 +0800

    commit

--- spring-step/src/main/java/cn/bugstack/springframework/beans/factory/config/AutowireCapableBeanFactory.java
+++ spring-step/src/main/java/cn/bugstack/springframework/beans/factory/config/AutowireCapableBeanFactory.java
@@ -8,7 +8,7 @@ import cn.bugstack.springframework.beans.factory.BeanFactory;
  *
  *
  * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
- * @description Extension of the {@link BeanFactory}
+ * @description Extension of the {@link cn.bugstack.springframework.beans.factory.BeanFactory}
  * interface to be implemented by bean factories that are capable of
  * autowiring, provided that they want to expose this functionality for
  * existing bean instances.
--- spring-step/src/main/java/cn/bugstack/springframework/context/ConfigurableApplicationContext.java
+++ spring-step/src/main/java/cn/bugstack/springframework/context/ConfigurableApplicationContext.java
@@ -10,7 +10,7 @@ import cn.bugstack.springframework.beans.BeansException;
  * @description SPI 接口配置应用上下文 SPI interface to be implemented by most if not all application contexts.
  * Provides facilities to configure an application context in addition
  * to the application context client methods in the
- * {@link ApplicationContext} interface.
+ * {@link cn.bugstack.springframework.context.ApplicationContext} interface.
  * @date 2022/3/10
  *
  *
