e5e5fbee7a5138cb2feca2438345e3fc984da660
yixianghsy <yixianghsy@gmail.com>
2024-12-30 11:20:26 +0800

    03

---
+++ spring-step/src/main/java/cn/bugstack/springframework/beans/factory/support/SimpleInstantiationStrategy.java
@@ -0,0 +1,44 @@
+package cn.bugstack.springframework.beans.factory.support;
+
+import cn.bugstack.springframework.beans.BeansException;
+import cn.bugstack.springframework.beans.factory.config.BeanDefinition;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ *
+ *
+ *
+ * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
+ * @description JDK实例化策略
+ * @date 2022/03/08
+ *
+ *
+ */
+public class SimpleInstantiationStrategy implements InstantiationStrategy {
+    /**
+     *
+     * @param beanDefinition
+     * @param beanName
+     * @param ctor  获取与入参信息相对应的构造函数
+     * @param args
+     * @return
+     * @throws BeansException
+     */
+    @Override
+    public Object instantiate(BeanDefinition beanDefinition, String beanName, Constructor ctor, Object[] args) throws BeansException {
+        Class clazz = beanDefinition.getBeanClass();
+        try {
+            if (null != ctor) {
+                //入参信息传递给newInstance进行实例化
+                return clazz.getDeclaredConstructor(ctor.getParameterTypes()).newInstance(args);
+            } else {
+                return clazz.getDeclaredConstructor().newInstance();
+            }
+        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new BeansException("Failed to instantiate [" + clazz.getName() + "]", e);
+        }
+    }
+
+}
