520062b3ed8faa6b070012cad599c54b2b68632a
yixianghsy <yixianghsy@gmail.com>
2025-01-03 09:55:19 +0800

    11

--- spring-step/src/test/java/cn/bugstack/springframework/test/bean/UserService.java
+++ spring-step/src/test/java/cn/bugstack/springframework/test/bean/UserService.java
@@ -1,50 +1,25 @@
 package cn.bugstack.springframework.test.bean;
 
-/**
- * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- * 公众号：bugstack虫洞栈
- * Create by 小傅哥(fustack)
- */
-public class UserService {
+import java.util.Random;
 
-    private String uId;
-    private String company;
-    private String location;
-    private IUserDao userDao;
+public class UserService implements IUserService {
 
     public String queryUserInfo() {
-        return userDao.queryUserName(uId) + "," + company + "," + location;
+        try {
+            Thread.sleep(new Random(1).nextInt(100));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "小傅哥，100001，深圳";
     }
 
-    public String getuId() {
-        return uId;
+    public String register(String userName) {
+        try {
+            Thread.sleep(new Random(1).nextInt(100));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "注册用户：" + userName + " success！";
     }
 
-    public void setuId(String uId) {
-        this.uId = uId;
-    }
-
-    public String getCompany() {
-        return company;
-    }
-
-    public void setCompany(String company) {
-        this.company = company;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public IUserDao getUserDao() {
-        return userDao;
-    }
-
-    public void setUserDao(IUserDao userDao) {
-        this.userDao = userDao;
-    }
 }
